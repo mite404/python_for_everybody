27de69d50f8ec2c5bfdff09b34246c24365a0558
mite404 <ethana2008@gmail.com>
2023-12-02 16:58:53 -0500

    slicing strings

---
+++ chapter_06/06_02_slicing_strings.py
@@ -0,0 +1,19 @@
+# Slicing strings in Python
+
+
+# ex 1
+s = 'Monty Python'
+print(s[0:4])
+
+print(s[6:7])
+
+print(s[6:20])
+
+
+# ex 2 omitting portions of a string
+s = 'Monty Python'
+print(s[:2])
+
+print(s[8:])
+
+print(s[:])     # whole string
