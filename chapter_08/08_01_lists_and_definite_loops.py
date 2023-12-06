3e88ee423f6661bb9917afe4281c8731a2a96ba5
mite404 <ethana2008@gmail.com>
2023-12-06 12:19:36 -0500

    continuing learning lists

---
+++ chapter_08/08_01_lists_and_definite_loops.py
@@ -0,0 +1,27 @@
+# Lists and Definite Loops
+
+friends = ['Joeseph', 'Glenn', 'Sally']
+for friends in friends:
+    print('Happy New Year: ', friends)
+print('done.')
+
+
+# looking inside lists
+friends = [ 'Joeseph', 'Glenn', 'Sally' ]
+print(friends[1])
+
+# how long is a list?
+greet = 'Hello Bob'
+print(len(greet))
+
+x = [1, 2, 'joe', 99]
+print(len(x))
+
+
+# using the 'range' function to loop
+print(range(4))
+
+friends = [ 'Joeseph', 'Glenn', 'Sally' ]
+print(len(friends))
+
+print(range(len(friends)))
